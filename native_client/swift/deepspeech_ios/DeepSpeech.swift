1f818d786d2c7db3e00d93f0a414f3f1bb4897f8
Reuben Morais <reuben.morais@gmail.com>
2020-07-08 17:09:50 +0200

    Finish wrapping all API endpoints and Metadata structs

--- native_client/swift/deepspeech_ios/DeepSpeech.swift
+++ native_client/swift/deepspeech_ios/DeepSpeech.swift
@@ -6,136 +6,278 @@
 //  Copyright © 2020 Mozilla. All rights reserved.
 //
 
-import Foundation
-import AVFoundation
-import AudioToolbox
-import Accelerate
-
 import deepspeech_ios.libdeepspeech_Private
 
-/// Holds audio information used for building waveforms
-final class AudioContext {
+public enum DeepSpeechError: Error {
+    // Should be kept in sync with
+    case noModel(errorCode: Int32)
+    case invalidAlphabet(errorCode: Int32)
+    case invalidShape(errorCode: Int32)
+    case invalidScorer(errorCode: Int32)
+    case modelIncompatible(errorCode: Int32)
+    case scorerNotEnabled(errorCode: Int32)
+    case scorerUnreadable(errorCode: Int32)
+    case scorerInvalidLm(errorCode: Int32)
+    case scorerNoTrie(errorCode: Int32)
+    case scorerInvalidTrie(errorCode: Int32)
+    case scorerVersionMismatch(errorCode: Int32)
+    case failInitMmap(errorCode: Int32)
+    case failInitSess(errorCode: Int32)
+    case failInterpreter(errorCode: Int32)
+    case failRunSess(errorCode: Int32)
+    case failCreateStream(errorCode: Int32)
+    case failReadProtobuf(errorCode: Int32)
+    case failCreateSess(errorCode: Int32)
+    case failCreateModel(errorCode: Int32)
 
-    /// The audio asset URL used to load the context
-    public let audioURL: URL
+    // Additional case for invalid error codes, should never happen unless the user has mixed header and binary versions
+    case invalidErrorCode(errorCode: Int32)
+}
 
-    /// Total number of samples in loaded asset
-    public let totalSamples: Int
-
-    /// Loaded asset
-    public let asset: AVAsset
-
-    // Loaded assetTrack
-    public let assetTrack: AVAssetTrack
-
-    private init(audioURL: URL, totalSamples: Int, asset: AVAsset, assetTrack: AVAssetTrack) {
-        self.audioURL = audioURL
-        self.totalSamples = totalSamples
-        self.asset = asset
-        self.assetTrack = assetTrack
-    }
-
-    public static func load(fromAudioURL audioURL: URL, completionHandler: @escaping (_ audioContext: AudioContext?) -> ()) {
-        let asset = AVURLAsset(url: audioURL, options: [AVURLAssetPreferPreciseDurationAndTimingKey: NSNumber(value: true as Bool)])
-
-        guard let assetTrack = asset.tracks(withMediaType: AVMediaType.audio).first else {
-            fatalError("Couldn't load AVAssetTrack")
-        }
-
-        asset.loadValuesAsynchronously(forKeys: ["duration"]) {
-            var error: NSError?
-            let status = asset.statusOfValue(forKey: "duration", error: &error)
-            switch status {
-            case .loaded:
-                guard
-                    let formatDescriptions = assetTrack.formatDescriptions as? [CMAudioFormatDescription],
-                    let audioFormatDesc = formatDescriptions.first,
-                    let asbd = CMAudioFormatDescriptionGetStreamBasicDescription(audioFormatDesc)
-                    else { break }
-
-                let totalSamples = Int((asbd.pointee.mSampleRate) * Float64(asset.duration.value) / Float64(asset.duration.timescale))
-                let audioContext = AudioContext(audioURL: audioURL, totalSamples: totalSamples, asset: asset, assetTrack: assetTrack)
-                completionHandler(audioContext)
-                return
-
-            case .failed, .cancelled, .loading, .unknown:
-                print("Couldn't load asset: \(error?.localizedDescription ?? "Unknown error")")
-            }
-
-            completionHandler(nil)
+extension DeepSpeechError : LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .noModel(let errorCode),
+             .invalidAlphabet(let errorCode),
+             .invalidShape(let errorCode),
+             .invalidScorer(let errorCode),
+             .modelIncompatible(let errorCode),
+             .scorerNotEnabled(let errorCode),
+             .scorerUnreadable(let errorCode),
+             .scorerInvalidLm(let errorCode),
+             .scorerNoTrie(let errorCode),
+             .scorerInvalidTrie(let errorCode),
+             .scorerVersionMismatch(let errorCode),
+             .failInitMmap(let errorCode),
+             .failInitSess(let errorCode),
+             .failInterpreter(let errorCode),
+             .failRunSess(let errorCode),
+             .failCreateStream(let errorCode),
+             .failReadProtobuf(let errorCode),
+             .failCreateSess(let errorCode),
+             .failCreateModel(let errorCode),
+             .invalidErrorCode(let errorCode):
+            let result = DS_ErrorCodeToErrorMessage(errorCode)
+            defer { DS_FreeString(result) }
+            return String(cString: result!)
         }
     }
 }
 
-func render(audioContext: AudioContext?, stream: OpaquePointer) {
-    guard let audioContext = audioContext else {
-        fatalError("Couldn't create the audioContext")
-    }
-
-    let sampleRange: CountableRange<Int> = 0..<audioContext.totalSamples
-
-    guard let reader = try? AVAssetReader(asset: audioContext.asset)
-        else {
-            fatalError("Couldn't initialize the AVAssetReader")
-    }
-
-    reader.timeRange = CMTimeRange(start: CMTime(value: Int64(sampleRange.lowerBound), timescale: audioContext.asset.duration.timescale),
-                                   duration: CMTime(value: Int64(sampleRange.count), timescale: audioContext.asset.duration.timescale))
-
-    let outputSettingsDict: [String : Any] = [
-        AVFormatIDKey: Int(kAudioFormatLinearPCM),
-        AVLinearPCMBitDepthKey: 16,
-        AVLinearPCMIsBigEndianKey: false,
-        AVLinearPCMIsFloatKey: false,
-        AVLinearPCMIsNonInterleaved: false
-    ]
-
-    let readerOutput = AVAssetReaderTrackOutput(track: audioContext.assetTrack,
-                                                outputSettings: outputSettingsDict)
-    readerOutput.alwaysCopiesSampleData = false
-    reader.add(readerOutput)
-
-    var sampleBuffer = Data()
-
-    // 16-bit samples
-    reader.startReading()
-    defer { reader.cancelReading() }
-
-    while reader.status == .reading {
-        guard let readSampleBuffer = readerOutput.copyNextSampleBuffer(),
-            let readBuffer = CMSampleBufferGetDataBuffer(readSampleBuffer) else {
-                break
-        }
-        // Append audio sample buffer into our current sample buffer
-        var readBufferLength = 0
-        var readBufferPointer: UnsafeMutablePointer<Int8>?
-        CMBlockBufferGetDataPointer(readBuffer,
-                                    atOffset: 0,
-                                    lengthAtOffsetOut: &readBufferLength,
-                                    totalLengthOut: nil,
-                                    dataPointerOut: &readBufferPointer)
-        sampleBuffer.append(UnsafeBufferPointer(start: readBufferPointer, count: readBufferLength))
-        CMSampleBufferInvalidate(readSampleBuffer)
-
-        let totalSamples = sampleBuffer.count / MemoryLayout<Int16>.size
-        print("read \(totalSamples) samples")
-        
-        sampleBuffer.withUnsafeBytes { (samples: UnsafeRawBufferPointer) in
-            let unsafeBufferPointer = samples.bindMemory(to: Int16.self)
-            let unsafePointer = unsafeBufferPointer.baseAddress!
-            DS_FeedAudioContent(stream, unsafePointer, UInt32(totalSamples))
-        }
-        
-        sampleBuffer.removeAll()
-    }
-
-    // if (reader.status == AVAssetReaderStatusFailed || reader.status == AVAssetReaderStatusUnknown)
-    guard reader.status == .completed else {
-        fatalError("Couldn't read the audio file")
+private func errorCodeToEnum(errorCode: Int32) -> DeepSpeechError {
+    switch Int(errorCode) {
+    case Int(DS_ERR_NO_MODEL.rawValue):
+        return DeepSpeechError.noModel(errorCode: errorCode)
+    case Int(DS_ERR_INVALID_ALPHABET.rawValue):
+        return DeepSpeechError.invalidAlphabet(errorCode: errorCode)
+    case Int(DS_ERR_INVALID_SHAPE.rawValue):
+        return DeepSpeechError.invalidShape(errorCode: errorCode)
+    case Int(DS_ERR_INVALID_SCORER.rawValue):
+        return DeepSpeechError.invalidScorer(errorCode: errorCode)
+    case Int(DS_ERR_MODEL_INCOMPATIBLE.rawValue):
+        return DeepSpeechError.modelIncompatible(errorCode: errorCode)
+    case Int(DS_ERR_SCORER_NOT_ENABLED.rawValue):
+        return DeepSpeechError.scorerNotEnabled(errorCode: errorCode)
+    case Int(DS_ERR_SCORER_UNREADABLE.rawValue):
+        return DeepSpeechError.scorerUnreadable(errorCode: errorCode)
+    case Int(DS_ERR_SCORER_INVALID_LM.rawValue):
+        return DeepSpeechError.scorerInvalidLm(errorCode: errorCode)
+    case Int(DS_ERR_SCORER_NO_TRIE.rawValue):
+        return DeepSpeechError.scorerNoTrie(errorCode: errorCode)
+    case Int(DS_ERR_SCORER_INVALID_TRIE.rawValue):
+        return DeepSpeechError.scorerInvalidTrie(errorCode: errorCode)
+    case Int(DS_ERR_SCORER_VERSION_MISMATCH.rawValue):
+        return DeepSpeechError.scorerVersionMismatch(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_INIT_MMAP.rawValue):
+        return DeepSpeechError.failInitMmap(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_INIT_SESS.rawValue):
+        return DeepSpeechError.failInitSess(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_INTERPRETER.rawValue):
+        return DeepSpeechError.failInterpreter(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_RUN_SESS.rawValue):
+        return DeepSpeechError.failRunSess(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_CREATE_STREAM.rawValue):
+        return DeepSpeechError.failCreateStream(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_READ_PROTOBUF.rawValue):
+        return DeepSpeechError.failReadProtobuf(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_CREATE_SESS.rawValue):
+        return DeepSpeechError.failCreateSess(errorCode: errorCode)
+    case Int(DS_ERR_FAIL_CREATE_MODEL.rawValue):
+        return DeepSpeechError.failCreateModel(errorCode: errorCode)
+    default:
+        return DeepSpeechError.invalidErrorCode(errorCode: errorCode)
     }
 }
 
-public class DeepSpeech {
+private func evaluateErrorCode(errorCode: Int32) throws {
+    if errorCode != Int32(DS_ERR_OK.rawValue) {
+        throw errorCodeToEnum(errorCode: errorCode)
+    }
+}
+
+public struct DeepSpeechTokenMetadata {
+    let text: String
+    let timestep: Int
+    let startTime: Float
+
+    internal init(fromInternal: TokenMetadata) {
+        text = String(cString: fromInternal.text)
+        timestep = Int(fromInternal.timestep)
+        startTime = fromInternal.start_time
+    }
+}
+
+public struct DeepSpeechCandidateTranscript {
+    private(set) var tokens: [DeepSpeechTokenMetadata] = []
+    let confidence: Double
+
+    internal init(fromInternal: CandidateTranscript) {
+        let tokensBuffer = UnsafeBufferPointer<TokenMetadata>(start: fromInternal.tokens, count: Int(fromInternal.num_tokens))
+        for tok in tokensBuffer {
+            tokens.append(DeepSpeechTokenMetadata(fromInternal: tok))
+        }
+        confidence = fromInternal.confidence
+    }
+}
+
+public struct DeepSpeechMetadata {
+    private(set) var transcripts: [DeepSpeechCandidateTranscript] = []
+
+    internal init(fromInternal: UnsafeMutablePointer<Metadata>) {
+        let md = fromInternal.pointee
+        let transcriptsBuffer = UnsafeBufferPointer<CandidateTranscript>(start: md.transcripts, count: Int(md.num_transcripts))
+
+        for tr in transcriptsBuffer {
+            transcripts.append(DeepSpeechCandidateTranscript(fromInternal: tr))
+        }
+    }
+}
+
+public class DeepSpeechStream {
+    private var streamCtx: OpaquePointer!
+
+    internal init(streamContext: OpaquePointer) {
+        streamCtx = streamContext
+    }
+
+    deinit {
+        if streamCtx != nil {
+            DS_FreeStream(streamCtx)
+            streamCtx = nil
+        }
+    }
+
+    public func feedAudioContent(buffer: Array<Int16>) {
+        precondition(streamCtx != nil, "calling method on invalidated Stream")
+
+        buffer.withUnsafeBufferPointer { unsafeBufferPointer in
+            feedAudioContent(buffer: unsafeBufferPointer)
+        }
+    }
+
+    public func feedAudioContent(buffer: UnsafeBufferPointer<Int16>) {
+        precondition(streamCtx != nil, "calling method on invalidated Stream")
+
+        DS_FeedAudioContent(streamCtx, buffer.baseAddress, UInt32(buffer.count))
+    }
+
+    public func intermediateDecode() -> String {
+        precondition(streamCtx != nil, "calling method on invalidated Stream")
+
+        let result = DS_IntermediateDecode(streamCtx)
+        defer { DS_FreeString(result) }
+        return String(cString: result!)
+    }
+
+    public func intermediateDecodeWithMetadata(numResults: Int) -> DeepSpeechMetadata {
+        precondition(streamCtx != nil, "calling method on invalidated Stream")
+        let result = DS_IntermediateDecodeWithMetadata(streamCtx, UInt32(numResults))!
+        defer { DS_FreeMetadata(result) }
+        return DeepSpeechMetadata(fromInternal: result)
+    }
+
+    public func finishStream() -> String {
+        precondition(streamCtx != nil, "calling method on invalidated Stream")
+
+        let result = DS_FinishStream(streamCtx)
+        defer {
+            DS_FreeString(result)
+            streamCtx = nil
+        }
+        return String(cString: result!)
+    }
+}
+
+public class DeepSpeechModel {
+    private var modelCtx: OpaquePointer!
+
+    public init(modelPath: String) throws {
+        let err = DS_CreateModel(modelPath, &modelCtx)
+        try evaluateErrorCode(errorCode: err)
+    }
+
+    deinit {
+        DS_FreeModel(modelCtx)
+        modelCtx = nil
+    }
+
+    public func getBeamWidth() -> Int {
+        return Int(DS_GetModelBeamWidth(modelCtx))
+    }
+
+    public func setBeamWidth(beamWidth: Int) throws {
+        let err = DS_SetModelBeamWidth(modelCtx, UInt32(beamWidth))
+        try evaluateErrorCode(errorCode: err)
+    }
+
+    public var sampleRate: Int {
+        get {
+            return Int(DS_GetModelSampleRate(modelCtx))
+        }
+    }
+
+    public func enableExternalScorer(scorerPath: String) throws {
+        let err = DS_EnableExternalScorer(modelCtx, scorerPath)
+        try evaluateErrorCode(errorCode: err)
+    }
+
+    public func disableExternalScorer() throws {
+        let err = DS_DisableExternalScorer(modelCtx)
+        try evaluateErrorCode(errorCode: err)
+    }
+
+    public func setScorerAlphaBeta(alpha: Float, beta: Float) throws {
+        let err = DS_SetScorerAlphaBeta(modelCtx, alpha, beta)
+        try evaluateErrorCode(errorCode: err)
+    }
+
+    public func speechToText(buffer: Array<Int16>) -> String {
+        return buffer.withUnsafeBufferPointer { unsafeBufferPointer -> String in
+            return speechToText(buffer: unsafeBufferPointer)
+        }
+    }
+
+    public func speechToText(buffer: UnsafeBufferPointer<Int16>) -> String {
+        let result = DS_SpeechToText(modelCtx, buffer.baseAddress, UInt32(buffer.count))
+        defer { DS_FreeString(result) }
+        return String(cString: result!)
+    }
+
+    public func speechToTextWithMetadata(buffer: Array<Int16>, numResults: Int) -> DeepSpeechMetadata {
+        return buffer.withUnsafeBufferPointer { unsafeBufferPointer -> DeepSpeechMetadata in
+            let result = DS_SpeechToTextWithMetadata(modelCtx, unsafeBufferPointer.baseAddress, UInt32(buffer.count), UInt32(numResults))!
+            defer { DS_FreeMetadata(result) }
+            return DeepSpeechMetadata(fromInternal: result)
+        }
+    }
+
+    public func createStream() throws -> DeepSpeechStream {
+        var streamContext: OpaquePointer!
+        let err = DS_CreateStream(modelCtx, &streamContext)
+        try evaluateErrorCode(errorCode: err)
+        return DeepSpeechStream(streamContext: streamContext)
+    }
+
     public class func open(path: String, scorerPath: Optional<String> = nil) -> OpaquePointer {
         var fooOpaque: OpaquePointer!
         DS_CreateModel(path, &fooOpaque)
@@ -144,73 +286,16 @@ public class DeepSpeech {
         }
         return fooOpaque
     }
-    
+
     public class func createStream(modelState: OpaquePointer) -> OpaquePointer {
         var fooOpaque: OpaquePointer!
         DS_CreateStream(modelState, &fooOpaque)
         return fooOpaque
     }
+}
 
-    public class func test(modelState: OpaquePointer, audioPath: String, completion: @escaping () -> ()) {
-        let url = URL(fileURLWithPath: audioPath)
-
-        //var format = AudioStreamBasicDescription.init()
-        //format.mSampleRate = 16000;
-        //format.mFormatID = kAudioFormatLinearPCM;
-        //format.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsPacked;
-        //format.mBitsPerChannel = 16;
-        //format.mChannelsPerFrame = 1;
-        //format.mBytesPerFrame = format.mChannelsPerFrame * format.mBitsPerChannel / 8;
-        //format.mFramesPerPacket = 1;
-        //format.mBytesPerPacket = format.mFramesPerPacket * format.mBytesPerFrame;
-        //
-        //var file = Optional<ExtAudioFileRef>.init(nilLiteral: ());
-        //let status = ExtAudioFileCreateWithURL(url as CFURL,
-        //                                       kAudioFileWAVEType,
-        //                                       &format,
-        //                                       nil,
-        //                                       0,
-        //                                       &file)
-        //print("status: \(status)")
-        //let status2 = ExtAudioFileSetProperty(file!,
-        //                                     kExtAudioFileProperty_ClientDataFormat,
-        //                                     UInt32(MemoryLayout<AudioStreamBasicDescription>.size),
-        //                                     &format)
-        //print("status: \(status2)")
-        //
-        //ExtAudioFileRead(file, <#T##ioNumberFrames: UnsafeMutablePointer<UInt32>##UnsafeMutablePointer<UInt32>#>, <#T##ioData: UnsafeMutablePointer<AudioBufferList>##UnsafeMutablePointer<AudioBufferList>#>)
-        
-        let stream = createStream(modelState: modelState)
-        print("\(audioPath)")
-        let start = CFAbsoluteTimeGetCurrent()
-        AudioContext.load(fromAudioURL: url, completionHandler: { audioContext in
-            guard let audioContext = audioContext else {
-                fatalError("Couldn't create the audioContext")
-            }
-            render(audioContext: audioContext, stream: stream)
-            DS_FinishStream(stream)
-            let end = CFAbsoluteTimeGetCurrent()
-            print("\"\(audioPath)\": \(end - start)")
-            completion()
-        })
-        
-        //let file = try! AVAudioFile(forReading: url)
-        //print("file length \(file.length)")
-        //let format = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: 16000, channels: 1, interleaved: false)!
-        //let stream = createStream(modelState: modelState)
-        //while file.framePosition < file.length {
-        //    let pcmBuf = AVAudioPCMBuffer.init(pcmFormat: format, frameCapacity: 8 * 1024)! // arbitrary frameCapacity
-        //    try! file.read(into: pcmBuf)
-        //    if pcmBuf.frameLength == 0 {
-        //        break
-        //    }
-        //    print("read \(pcmBuf.frameLength) frames into buffer")
-        //    let rawPtr = pcmBuf.audioBufferList.pointee.mBuffers.mData!
-        //    let ptr = rawPtr.bindMemory(to: Int16.self, capacity: Int(pcmBuf.frameLength))
-        //    print("first few samples: \(ptr[0]) \(ptr[1]) \(ptr[2]) \(ptr[3]) ")
-        //    DS_FeedAudioContent(stream, ptr, UInt32(pcmBuf.frameLength))
-        //}
-        //let result = DS_FinishStream(stream)
-        //return String.init(cString: result!)
-    }
+public func DeepSpeechVersion() -> String {
+    let result = DS_Version()
+    defer { DS_FreeString(result) }
+    return String(cString: result!)
 }
